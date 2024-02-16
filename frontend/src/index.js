8b855f4280f054ee00c96c135962eac56118f883
tomasdifelice01 <difelice@stanford.edu>
2024-02-16 13:18:02 -0800

    adding react-boostrap
    removing bootstrap and adding react-boostrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap"; // Import necessary components from react-bootstrap
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<StrictMode>
+		<Container>
+			{" "}
+			{/* Use Container from react-bootstrap */}
+			<App />
+		</Container>
+	</StrictMode>
 );
