85992461a10118906e526aed4b4ed3738dd68fc9
Steven G. Opferman <steven.g.opferman@gmail.com>
2024-02-17 14:42:26 -0800

    react-bootstrap works now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap"; // Import necessary components from react-bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
 	<StrictMode>
 		<Container>
