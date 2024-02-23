b4ab164a5e01fe1238949f500115a02cd573f21f
simrantan <simrant@stanford.edu>
2024-02-23 12:45:35 -0800

    color

---
+++ frontend/src/theme/themes.js
@@ -0,0 +1,19 @@
+import { palette } from "./colors";
+
+const theme = {
+	colors: {
+		background: palette.white,
+		accentPurple: palette.purple,
+		accentPink: palette.pink,
+		accentRed: palette.red,
+		accentBlue: palette.blue,
+		accentBlack: palette.black,
+	},
+	components: {
+		Text: {
+			style: {
+				fontFamily: "Montserrat_400Regular",
+			},
+		},
+	},
+};
