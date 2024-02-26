57e046452eabc00ec0e6a7231f4abf1cc7c25402
simrantan <simrant@stanford.edu>
2024-02-25 20:40:54 -0800

    added fonts

---
+++ frontend/src/theme/fonts.js
@@ -0,0 +1,19 @@
+export const fontStyles = `
+  @font-face {
+    font-family: 'Karla';
+    src: url('./assets/karla/Karla-Regular.ttf') format('ttf');
+    /* Add additional formats and styles (e.g., bold, italic) as needed */
+  }
+
+  @font-face {
+    font-family: 'Inter';
+    src: url('./assets/inter/Inter-Regular.ttf') format('ttf');
+    /* Add additional formats and styles (e.g., bold, italic) as needed */
+  }
+
+  @font-face {
+    font-family: 'Inter-Semi-Bold';
+    src: url('./assets/inter/Inter-Semi_Bold.ttf') format('ttf');
+    /* Add additional formats and styles (e.g., bold, italic) as needed */
+  }
+`;
