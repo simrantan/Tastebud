4122ad3718162ad1338d85e464702e96e0a42bc5
Steven G. Opferman <steven.g.opferman@gmail.com>
2024-03-01 13:21:35 -0800

    can now read a detailed recipe from Firebase

--- backend/index.js
+++ backend/index.js
@@ -102,17 +102,23 @@ app.get("/recipe_book/:userId", async (req, res) => {
 
 /* ########################### Recipe ########################## */
 /** Get information for a single recipe */
-app.get("/recipe/:recipeId", (req, res) => {
-	const recipeId = Number(req.params.recipeId);
+app.get("/user/:userId/recipe/:recipeId", async (req, res) => {
+	const userId = req.params.userId;
+	const recipeId = req.params.recipeId;
 
-	res.json({
-		id: recipeId,
-		name: "Banana Bread",
-		chat_id: 111,
-		text: "## Banana Bread Recipe\n\n**Ingredients:**\n- 2 to 3 ripe bananas, mashed\n- 1/3 cup (75g) melted butter\n- 1 teaspoon baking soda\n- Pinch of salt\n- 3/4 cup (150g) sugar\n- 1 large egg, beaten\n- 1 teaspoon vanilla extract\n- 1 1/2 cups (190g) all-purpose flour\n\n**Optional Add-ins:**\n- 1/2 cup (50g) chopped nuts (walnuts or pecans)\n- 1/2 cup (80g) chocolate chips\n\n**Instructions:**\n1. Preheat your oven to 350°F (175°C). Grease a 9x5-inch loaf pan.\n2. In a mixing bowl, mash the ripe bananas with a fork until smooth.\n3. Stir the melted butter into the mashed bananas.\n4. Mix in the baking soda and salt.\n5. Stir in the sugar, beaten egg, and vanilla extract.\n6. Mix in the flour until just combined. Do not overmix.\n7. If using, fold in the chopped nuts or chocolate chips.\n8. Pour the batter into the prepared loaf pan.\n9. Bake in the preheated oven for 50 to 60 minutes, or until a toothpick inserted into the center comes out clean.\n10. Remove the banana bread from the oven and let it cool in the pan for 10 minutes.\n11. Transfer the bread to a wire rack to cool completely before slicing.",
-		picture_url: "https://placekitten.com/1000/1000",
-		cuisine: "American",
-	});
+	try {
+		const docRef = doc(DATABASE, "users", userId, "recipes", recipeId);
+		const docSnap = await getDoc(docRef);
+
+		if (docSnap.exists()) {
+			res.json(docSnap.data());
+		} else {
+			console.log("can't find recipe");
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Internal Server Error");
+	}
 });
 
 /* ########################### Chat ########################## */
