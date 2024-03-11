99e1c34f07b374dd479be847564a6a4ff4a676fa
simrantan <simrant@stanford.edu>
2024-03-11 01:23:45 -0700

    deleting random junk

--- frontend/src/components/ChatsMain.jsx
+++ frontend/src/components/ChatsMain.jsx
@@ -250,7 +250,6 @@ export default function ChatsMain() {
 	return (
 		<Container fluid className="py-5" style={{ backgroundColor: "#eee" }}>
 			<Row className="justify-content-center">
-				{/* RecipePanel with a placeholder recipe */}
 				<Col md="4" lg="3" xl="2">
 					<RecipePanel recipe={selectedRecipe} />
 				</Col>
