7f4e4acc4c2969290a27048100acdd002449307c
tomasdifelice01 <difelice@stanford.edu>
2024-03-17 10:50:06 -0700

    fixing function call

--- frontend/src/components/ChatsMain.jsx
+++ frontend/src/components/ChatsMain.jsx
@@ -330,7 +330,7 @@ export default function ChatsMain() {
 									</div>
 								))}
 
-							{receivedIsRecipeList(
+							{receivedIsRecipeList ?? (
 								<RecipeCarousel
 									recipes={recipePanelData.recipes}
 									onRecipeClick={(index) => {
