4be0bcaec7667b86ef934e0a45f009c44d67fd6f
simrantan <simrant@stanford.edu>
2024-03-17 11:19:00 -0700

    Update ChatsMain.jsx

--- frontend/src/components/ChatsMain.jsx
+++ frontend/src/components/ChatsMain.jsx
@@ -329,7 +329,7 @@ export default function ChatsMain() {
 									</div>
 								))}
 
-							{receivedIsRecipeList ?? (
+							{receivedIsRecipeList && (
 								<RecipeCarousel
 									recipes={recipePanelData.recipes}
 									onRecipeClick={(index) => {
